aed47b30d22a8c5920a7740ac8584176c5317f19
Mike Wislek <mike@launchpadlab.com>
2021-11-18 19:15:05 -0600

    fix: create separate command for Ionic apps

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # lp-subsection-generator
 
-Generate subsections for react-redux apps.
+Generate subsections for react-redux and Ionic apps.
 Each subsection will contain components, forms, views and routes (see template folder for details).
 
 Remember to include your generated subsection in your top-level reducer and routes files!
 
-## Usage
+## Client Template Usage
 
 You can run the `generate-subsection` command using `yarn`:
 ```
@@ -13,10 +13,24 @@ You can run the `generate-subsection` command using `yarn`:
 ```
 
 ### Arguments
-  - `subsection-name`: the name of the subsection (optional, defaults to 'sub-section').
+  - `subsection-name`: the name of the subsection (required).
   - `destination`: the relative path to the directory where you'd like the subsection to be generated (optional, defaults to `./src/js/main/`).
 
 ### Options
   - `-V, --version`: the version number this utility
-  - `-t, --template-type <client | ionic>`: the subsection template type (default: "client")
+  - `-h, --help`: display help for command
+
+## Ionic Client Template Usage
+
+You can run the `generate-ionic-subsection` command using `yarn`:
+```
+> yarn generate-ionic-subsection [options] [subsection-name] [destination]
+```
+
+### Arguments
+  - `subsection-name`: the name of the subsection (required).
+  - `destination`: the relative path to the directory where you'd like the subsection to be generated (optional, defaults to `./src/js/main/`).
+
+### Options
+  - `-V, --version`: the version number this utility
   - `-h, --help`: display help for command
---
+++ generate.js
@@ -0,0 +1,71 @@
+/* eslint-disable no-console */
+const fs = require('fs-extra')
+const glob = require('glob')
+const path = require('path')
+const pluralize = require('pluralize')
+const { camelCase, kebabCase, flow, snakeCase, toUpper } = require('lodash')
+const screamingSnakeCase = flow(snakeCase, toUpper)
+
+function generate (subSectionName, destination, options) {
+  // Compute all casings
+  const singularName = pluralize.singular(subSectionName)
+  const camelCaseName = camelCase(singularName)
+  const kebabCaseName = kebabCase(singularName)
+  const pascalCaseName = pascalCase(singularName)
+  const screamingSnakeCaseName = screamingSnakeCase(singularName)
+  // Compute paths
+  const packagePath = getPackagePath()
+  const root = process.cwd()
+  const templatePath = templatePathFromTemplateType(options)
+  // GENERATE
+  console.log('Generating...')
+  // Copy files over
+  fs.copySync(path.resolve(packagePath, templatePath), path.resolve(packagePath, `./${ subSectionName }`))
+  // Replace template variables in files
+  const allFiles = glob.sync(path.resolve(packagePath, `./${ subSectionName }/**/*.js`))
+  allFiles.forEach(file => {
+    const template = fs.readFileSync(file, 'utf8')
+    const result = template
+      .replace(/%sub-section%/g, kebabCaseName)
+      .replace(/%subSection%/g, camelCaseName)
+      .replace(/%SubSection%/g, pascalCaseName)
+      .replace(/%SUB_SECTION%/g, screamingSnakeCaseName)
+      .replace(/%sub-sections%/g, pluralize(kebabCaseName))
+      .replace(/%subSections%/g, pluralize(camelCaseName))
+      .replace(/%SubSections%/g, pluralize(pascalCaseName))
+      .replace(/%SUB_SECTIONS%/g, pluralize(screamingSnakeCaseName))
+    return fs.writeFileSync(file, result)
+  })
+  // Rename view files
+  fs.renameSync(path.resolve(packagePath, `./${ subSectionName }/views/SubSections.js`), path.resolve(packagePath, `./${ subSectionName }/views/${ pluralize(pascalCaseName) }.js`))
+  fs.renameSync(path.resolve(packagePath, `./${ subSectionName }/views/SubSectionShow.js`), path.resolve(packagePath, `./${ subSectionName }/views/${ pascalCaseName }Show.js`))
+  // Move to final dest
+  fs.moveSync(path.resolve(packagePath, `./${ subSectionName }`), path.resolve(root, destination, subSectionName))
+  console.log('Done!')
+}
+
+module.exports = generate
+
+function getPackagePath () {
+  try {
+    return path.resolve(require.resolve('@launchpadlab/lp-subsection-generator'), '../')
+  } catch (e) {
+    console.log('Path not found, running locally')
+    return __dirname
+  }
+}
+
+function pascalCase (str) {
+  const cased = camelCase(str)
+  return cased.charAt(0).toUpperCase() + cased.slice(1)
+}
+
+function templatePathFromTemplateType(templateType) {
+  switch(templateType) {
+    case "client": return './template'
+    case "ionic": return './ionic-template'
+
+    default:
+      throw new Error (`generate-subsection: [ERROR]: unknown template type [${templateType}]`)
+  }
+}
