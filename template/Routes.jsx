fd05dc781e64eba3df90c40ad30495f694e47123
Conor Hawes <conor@launchpadlab.com>
2024-06-26 11:17:50 -0400

    Initial commit

--- template/Routes.jsx
+++ template/Routes.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 // import PropTypes from 'prop-types'
 import { Route, Switch, useRouteMatch } from 'react-router-dom'
 import * as Views from './views'
--- template/views/SubSectionShow.jsx
+++ template/views/SubSectionShow.jsx
@@ -1,4 +1,4 @@
-import React /*, { useEffect } */ from 'react'
+// import { useEffect } from 'react'
 // import PropTypes from 'prop-types'
 // import * as Types from 'types'
 // import { useDispatch, useSelector } from 'react-redux'
