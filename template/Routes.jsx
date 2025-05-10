01c681862fbbcc1f550c5940467dd912e3667e6f
Mike Wislek <mike@launchpadlab.com>
2025-05-09 19:23:13 -0500

    fix: remove additional spaces added by formatter

--- template/Routes.jsx
+++ template/Routes.jsx
@@ -2,7 +2,7 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 
 import %SubSections% from './views/%SubSections%.jsx'
-import % SubSection % Show from './views/%SubSection%Show.jsx'
+import %SubSection%Show from './views/%SubSection%Show.jsx'
 
 import Layout from './Layout.jsx'
 
@@ -12,8 +12,8 @@ function Routes () {
   return (
     <Routes>
       <Route element={<Layout />}>
-        <Route index element={<% SubSections % />} />
-        <Route path="/:id" element={<% SubSection % Show />} />
+        <Route index element={<%SubSections% />} />
+        <Route path="/:id" element={<%SubSection%Show />} />
         <Route path="*" element={<Navigate to=".." replace />} />
       </Route>
     </Routes>
